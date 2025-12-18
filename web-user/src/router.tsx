0840a8a525e17b292733d9cb0b965a4ad21ba6a9
Mohammed Famiz <111053856+famiz6571@users.noreply.github.com>
2025-12-18 20:34:11 +0400

    user app

---
+++ web-user/src/router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
